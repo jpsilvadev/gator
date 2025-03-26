4ad7ef2fe154788bfb378bd1156b3f2eea30db34
jpsilvadev <jpsjoaosilva98@gmail.com>
2025-03-26 23:06:34 +0000

    feat: unfollow a feed

--- handlers.go
+++ handlers.go
@@ -218,3 +218,25 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 	return nil
 
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <feed url>", cmd.Name)
+	}
+
+	url := cmd.Args[0]
+
+	feed, err := s.db.GetFeedByURL(context.Background(), url)
+	if err != nil {
+		return fmt.Errorf("failed to get feed: %v", err)
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to unfollow feed: %v", err)
+	}
+	return nil
+}
